1a58f45071209b1bb7314675348605da06ba124b
Swapnil <spchavhan16@gmail.com>
2025-01-19 23:17:31 +0530

    updated GitTest file

--- src/test/java/PatternsPrintPrograms/GitTest.java
+++ src/test/java/PatternsPrintPrograms/GitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class GitTest {
 
 	@Test
-	public static void main(String[] args) {
+	public void testExample() {
 		// TODO Auto-generated method stub
 		System.out.println("Git Test 1");
 	}
